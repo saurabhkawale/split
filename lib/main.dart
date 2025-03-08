e82dc0d69adbd619413dc2ab53403d48ec8a0f3d
s.jayant.kawale <s.jayant.kawale@accenture.com>
2025-03-08 14:42:40 +0530

    FIrst commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(APP());
+}
+
+class APP extends StatelessWidget {
+  const APP({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
